a1c6c46af8a05a733606d8ec801e855786b5dc33
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:20:29 +0200

    core: Include QCOM/MTK targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

--- core/config.mk
+++ core/config.mk
@@ -1099,4 +1099,10 @@ endif
 # Include any vendor specific config.mk file
 -include vendor/*/build/core/config.mk
 
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/sacred/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/sacred/build/core/mtk_target.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
