0e142cb3bb761b2691c85f4d053bb253308169e1
Alex Cruz <du.alexcruz@gmail.com>
2018-01-25 21:26:21 +0530

    Theme DUI dialogs & things [2/2]
    Change-Id: Ic02c05c9c08e027b8bfff415c5d49ee643ffbbda

--- target/product/core.mk
+++ target/product/core.mk
@@ -51,6 +51,8 @@ PRODUCT_PACKAGES += \
     Settings \
     SettingsBlackThemeOverlay \
     SettingsDarkThemeOverlay \
+    DuiBlackTheme \
+    DuiDarkTheme \
     SharedStorageBackup \
     StorageManager \
     SubstratumService \
